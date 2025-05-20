392237f604e586c45355a0ec69302dc84aa7af8a
juliaam1 <julia.arellano@estudiantat.upc.edu>
2025-05-20 17:01:18 +0200

    intento de correccion

--- Assets/Scripts/DispensadorGelat.cs
+++ Assets/Scripts/DispensadorGelat.cs
@@ -18,13 +18,25 @@ public class DispensadorGelat : MonoBehaviour
 
     }
 
-
-
     public void OnTriggerEnter(Collider gelat)
     {
-
         FoodStatus _statusFood = gelat.GetComponent<FoodStatus>();
-        _statusFood.isOnMachine = true;
-        
+        if (_statusFood != null)
+        {
+            _statusFood.isOnMachine = true;
+        }
+        else
+        {
+            Debug.LogWarning("El objeto que ha entrado no tiene el componente FoodStatus: " + gelat.name);
+        }
     }
+
+
+    //public void OnTriggerEnter(Collider gelat)
+    //{
+
+    //    FoodStatus _statusFood = gelat.GetComponent<FoodStatus>();
+    //    _statusFood.isOnMachine = true;
+
+    //}
 }
