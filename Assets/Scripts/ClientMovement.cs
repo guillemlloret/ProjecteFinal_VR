6431571b498a469dfed35d9d7fd6ad054c525501
guillemlloret <guillem.lloret.macia@estudiantat.upc.edu>
2025-06-01 11:19:09 +0200

    client rep menjar i marxa
    client rep menjar, es destrueix el menjar i  marxa i desapareix a la porta

--- Assets/Scripts/ClientMovement.cs
+++ Assets/Scripts/ClientMovement.cs
@@ -10,8 +10,10 @@ public class ClientMovement : MonoBehaviour
     public float speed = 2f;
 
     public  ClientSpawner spawner;
+    public bool orderCompleted = false;
 
     public static ClientMovement Instance;
+    public Transform target;
 
     public void SetWaypoints(Transform[] points)
     {
@@ -21,6 +23,32 @@ public class ClientMovement : MonoBehaviour
     void Awake()
     {
         Instance = this;
+    }
+
+    private void FixedUpdate()
+    {
+        //if (waypoints == null || currentWaypoint >= waypoints.Length) return;
+         target = waypoints[0];
+        
+        if (orderCompleted)
+        {
+            
+            Vector3 direction = (target.position - transform.position).normalized;
+            transform.position += direction * speed * Time.deltaTime;
+            //if (Vector3.Distance(transform.position, target.position) > 0.1f && orderCompleted)
+            //{
+            //    currentWaypoint++;
+            //}
+            Debug.Log(transform.position);
+            if (transform.position == target.position || transform.position.z >= target.position.z)
+            {
+                Debug.Log("arribat");
+                Destroy(ClientSpawner.Instance.client);
+            }
+        }
+        
+
+
     }
 
     void Update()
@@ -38,30 +66,58 @@ public class ClientMovement : MonoBehaviour
             currentWaypoint++;
         }
 
-       
+        
+
+
     }
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "tarrina" && ClientSpawner.Instance.client.tag == "tarrina")
         {
             Debug.Log("detectat");
-            Destroy(ClientSpawner.Instance.client);
+            //Destroy(ClientSpawner.Instance.client);
+            Destroy(other.gameObject);
+            orderCompleted = true;
+            //GetOut();
             
         }
 
         else if (other.gameObject.tag == "Polo_maduixa" && ClientSpawner.Instance.client.tag == "Polo_maduixa")
         {
             Debug.Log("detectat");
-            Destroy(ClientSpawner.Instance.client);
-          
+            //Destroy(ClientSpawner.Instance.client);
+            Destroy(other.gameObject);
+            orderCompleted = true;
+            //GetOut();
         }
 
         else if (other.gameObject.tag == "Polo_llimona" && ClientSpawner.Instance.client.tag == "Polo_llimona")
         {
             Debug.Log("detectat");
-            Destroy(ClientSpawner.Instance.client);
-           
+            //Destroy(ClientSpawner.Instance.client);
+            Destroy(other.gameObject);
+            orderCompleted = true;
+            //GetOut();
         }
     }
+    void GetOut()
+    {
+
+        if (orderCompleted == true)
+        {
+            Debug.Log("orderCompleted");
+            target = waypoints[0];
+            Debug.Log(target);
+        }
+
+        Vector3 direction = (target.position - transform.position).normalized;
+        transform.position += direction * speed * Time.deltaTime;
+        if (Vector3.Distance(transform.position, target.position) > 0.1f && orderCompleted)
+        {
+            currentWaypoint--;
+        }
+    }
+
+   
 }
