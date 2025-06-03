71ff935df0df0d423322466559651fc35a0087a6
eleniss <elena.fernandez.leon@estudiantat.upc.edu>
2025-06-03 02:08:57 +0200

    clients get out and slider works

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro; // text mesh pro
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private float gameTime;
     [SerializeField] TextMeshProUGUI timeTextBox;
+    public GameObject CompletedDay;
 
+    private bool timeUp = false;
 
-    void Start()
-    {
-        //timeTextBox = GameObject.Find("TimerText")?.GetComponent<TextMeshProUGUI>();
-    }
-
-    // Update is called once per frame
     void Update()
     {
         UpdateGameTimer();
@@ -22,15 +18,21 @@ public class GameManager : MonoBehaviour
 
     private void UpdateGameTimer()
     {
+        if (timeUp) return;
+
         gameTime -= Time.deltaTime;
+
+        if (gameTime <= 0f)
+        {
+            gameTime = 0f;
+            timeUp = true;
+            CompletedDay.SetActive(true);
+        }
+
         var minutes = Mathf.FloorToInt(gameTime / 60);
         var seconds = Mathf.FloorToInt(gameTime - minutes * 60);
 
-
-
         string gameTimeClockDisplay = string.Format("{0:0}:{1:0}", minutes, seconds);
-        
         timeTextBox.text = gameTimeClockDisplay;
     }
-
 }
